5d6ce90db5343751e0bcb6c4290d045f62878e1c
Nick Sheck <sheck@users.noreply.github.com>
2022-10-05 12:02:33 -0700

    Switch to typescript
    Based off of some code from:
    https://github.com/actions/typescript-action
    https://github.com/vercel/ncc

--- ncc/dist/index.js
+++ ncc/dist/index.js
@@ -1,10 +1,12 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
+
 // Nothing here yet
 
 module.exports = __webpack_exports__;
